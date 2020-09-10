a3aa270b8f7817394884beb7680a58c81105adfd
Michael Jones <acetousk@gmail.com>
2020-09-10 00:18:14 -0600

    ros_answers

--- include/FieldAnalyzer.h
+++ include/FieldAnalyzer.h
@@ -19,42 +19,5 @@
 
 #include "ChangeUp.h"
 
-class FieldAnalyzer{
-private:
-
-  ros::NodeHandle n;
-  ros::ServiceClient _getWorldPropertiesClient;
-  gazebo_msgs::GetWorldProperties _getWorldProperties;
-  ros::ServiceClient _getModelPropertiesClient;
-  gazebo_msgs::GetModelProperties _getModelProperties;
-  ros::ServiceClient _getModelStateClient;
-  gazebo_msgs::GetModelState _getModelState;
-
-  /*
-  visualization_msgs::MarkerArray _markers;
-  navigation::Field _field;
-  navigation::Action _action;
-
-  std_msgs::ColorRGBA black;
-  std_msgs::ColorRGBA grey;
-  std_msgs::ColorRGBA white;
-  std_msgs::ColorRGBA red;
-  std_msgs::ColorRGBA green;
-  std_msgs::ColorRGBA blue;
-  std_msgs::ColorRGBA purple;
-  std_msgs::ColorRGBA yellow;
-  std_msgs::ColorRGBA orange;
-//*/
-
-  FieldAnalyzer();
-  static FieldAnalyzer* _instance;
-
-public:
-
-  static FieldAnalyzer* get();
-
-//  void run();
-};
-
 
 #endif //NAVIGATION_ROBOTCONTROLLER_H
